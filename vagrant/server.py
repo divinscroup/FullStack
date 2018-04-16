fdd0ba58b63228835ae2df23da4a86f833fdee26
osama khatib <osamakhatib@outlook.com>
2018-04-17 01:17:16 +0300

    updating server and templates

--- vagrant/server.py
+++ vagrant/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -14,13 +14,14 @@ session = DBSession()
 @app.route('/')
 def home():
     return '<h2>Welcome Back !</h2>'
+
+
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurant_list(restaurant_id):
+def restaurant_menu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).first()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
 
     return render_template('menu.html', restaurant=restaurant, items=items)
-
 
 
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
@@ -29,22 +30,90 @@ def new_menu(restaurant_id):
         newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
-        return redirect(url_for('restaurant_list', restaurant_id=restaurant_id))
+        return redirect(url_for('restaurant_menu', restaurant_id=restaurant_id))
     else:
         return render_template('newMenu.html', restaurant_id=restaurant_id)
 
 
-
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def edit_menu(restaurant_id, menu_id):
-    return 'page to edit menu item.'
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+
+        editedItem.name = request.form['name']
+        editedItem.description = request.form['description']
+        editedItem.price = request.form['price']
+        editedItem.course = request.form['course']
+        session.add(editedItem)
+        session.commit()
+        flash("Menu Item has been edited")
+        return redirect(url_for('restaurant_menu', restaurant_id=restaurant_id))
+    else:
+
+        return render_template(
+            'editmenu.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
-def del_item(restaurant_id, menu_id):
-    return 'page to delete menu item.'
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    itemToDelete = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash("Menu Item has been deleted")
+        return redirect(url_for('restaurant_menu', restaurant_id=restaurant_id))
+    else:
+        return render_template('deletemenu.html', item=itemToDelete)
+"""
+description
+price
+course
+<dl>
+	<dt>Name:</dt>
+		<dd><input type ='text' size='30' name='name' placeholder = '{{item.name }}'></dd>
+	<dt>Description:</dt>
+		<dd><textarea name = 'description' rows='5' cols='40' placeholder = '{{item.description }}'></textarea></dd>
+	<dt>Price:</dt>
+		<dd><input type = 'text' size = '8' name='price' placeholder = '{{item.price}}'></dd>
+	<dt>Course:</dt>
 
+	{% if item.course == 'Appetizier' %}
+		<dd><input type ='radio' name='course' value='Appetizer'checked >Appetizer</dd>
+	{%else%}
+	<dd><input type ='radio' name='course' value='Appetizer' >Appetizer</dd>
+    {% endif %}
+
+    {% if item.course == 'Entree' %}
+       <dd><input type ='radio' name='course' value='Entree' checked>Entree</dd>
+
+    {%else%}
+		<dd><input type ='radio' name='course' value='Entree'>Entree</dd>
+    {% endif %}
+
+	{% if item.course == 'Dessert'%}
+		<dd><input type ='radio' name='course' value='Dessert'checked >Dessert</dd>
+
+	{% else %}
+	   <dd><input type ='radio' name='course' value='Dessert'>Dessert</dd>
+
+	{% endif %}
+
+
+	{%if item.course =='Beverage'%}
+		<dd><input type ='radio' name='course' value='Beverage'checked >Beverage</dd>
+	{% else %}
+		<dd><input type ='radio' name='course' value='Beverage'>Beverage</dd>
+	{% endif %}
+
+
+
+
+
+</dl>
+
+"""
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
